1d6203baa8ef24c3fa485fa59af3c5a4706aae3e
Romén Reyes-Peschl <seeduardo@zoho.com>
2018-09-27 13:31:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+console.log(string.toLowerCase())
+}
+
+var lowercase = "hello"
+var uppercase = "HELLO"
+var granlove = "I love you, Grandma."
